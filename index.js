0c566dac0f36c515396e9cc05142ff74714f8e38
JosueEspinoS <josue.espino.soto.git@gmail.com>
2022-04-21 20:23:34 -0500

    Agregando index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Spiderman = require('./app/spiderman.js');
+const andrewGarfield = new Spiderman("The Amazing Spiderman", 31, "Andrew Garfield", 3, "Sony")
+const tobeyMaguire = new Spiderman("Spiderman", 46, "Tobey Maguire", 4, "Sony")
+const tomHolland = new Spiderman("Spiderman UCM", 25, "Tom Holland", 3, "Marvel") 
+
+console.log(andrewGarfield);
+console.log(tobeyMaguire);
+console.log(tomHolland);
