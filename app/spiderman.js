5520e8995153adb29f34992a1bd5e488aaf0ad9a
JosueEspinoS <josue.espino.soto.git@gmail.com>
2022-04-21 19:38:56 -0500

    Creacion de la Clase Spiderman

---
+++ app/spiderman.js
@@ -0,0 +1,10 @@
+class Spiderman {
+    constructor (nombre, edad, actor, numPeliculas, estudio){
+        this.nombre = nombre
+        this.edad = edad
+        this.actor = actor
+        this.numPeliculas = numPeliculas
+        this.estudio = estudio
+    }
+}
+module.exports = Spiderman
